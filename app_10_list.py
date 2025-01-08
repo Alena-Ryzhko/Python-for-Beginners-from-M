84b18eb31e5ada0408ca993f855f6751d393a87f
“Alena-Ryzhko” <alenaryzhko1@gmail.com>
2025-01-07 23:31:12 -0500

    update

---
+++ app_10_list.py
@@ -0,0 +1,26 @@
+# Lists
+
+names = ["John", "Bob", "Mosh", "Sam", "Mary"]
+
+# 1.
+print(names)       # we'll get ['John', 'Bob', 'Mosh', 'Sam', 'Mary']
+
+# 2.
+# to get first element from this list:
+print(names[0])    # we'll get name "John" in terminal
+
+# 3.
+# to get last element from this list:
+print(names[-1])   # we'll get name "Mary" in terminal
+
+# 4.
+print(names[-2])   # we'll get name "Sam" in terminal
+
+# 5.
+# set a new value to list
+names[0] = "Jon"
+print(names )      # we'll get ['Jon', 'Bob', 'Mosh', 'Sam', 'Mary']
+
+# Range of list value
+print(names[0:3])      # we'll get a new list ['Jon', 'Bob', 'Mosh']
+
